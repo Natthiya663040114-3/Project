3b25c950d3f6c8302e118f8610f79c64f3155f08
Natthiya663040114-3 <Natthiya.c@kkumail.com>
2024-02-22 14:54:58 +0700

    booking

---
+++ cathotel/app/booking/user/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BookingUser from "./text"
+const bookingPage = () => {
+
+
+
+
+  return (
+        <>
+        <BookingUser/>
+        </>
+    );
+};
+
+export default bookingPage;
